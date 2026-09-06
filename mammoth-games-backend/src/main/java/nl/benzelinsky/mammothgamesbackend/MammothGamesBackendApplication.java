00b129b346d1b9872a84a6a6d5e41eb71aa400a1
ben <benjamin.zelinsky@gmail.com>
2026-09-06 15:41:44 +0200

    Add project files.

---
+++ mammoth-games-backend/src/main/java/nl/benzelinsky/mammothgamesbackend/MammothGamesBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.benzelinsky.mammothgamesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MammothGamesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MammothGamesBackendApplication.class, args);
+    }
+
+}
---
+++ mammoth-games-backend/src/test/java/nl/benzelinsky/mammothgamesbackend/MammothGamesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.benzelinsky.mammothgamesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MammothGamesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
